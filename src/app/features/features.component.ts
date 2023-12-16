6cee29e5c0addded876922d0eef77b1802da3c05
PatrycjaKa92 <patrycja-kawska@wp.pl>
2023-12-16 21:28:54 +0100

    changes of underline features

--- src/app/features/features.component.ts
+++ src/app/features/features.component.ts
@@ -1,16 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { BookmarkInOneClickComponent } from '../bookmark-in-one-click/bookmark-in-one-click.component';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-features',
   templateUrl: './features.component.html',
   styleUrls: ['./features.component.css'],
   standalone: true,
+  imports: [BookmarkInOneClickComponent, NgIf]
 })
 export class FeaturesComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  choice='simple bookmarks';
+
+  simpleBookmarks() {
+  this.choice='simple bookmarks';
+  }
+
+  speedySearching() {
+    this.choice='speedy searching';
+  }
+
+  easyScharing() {
+    this.choice='easy scharing';
   }
 
 }
