8c8a585925b212521a62b77dd9fe9292c7f0f64a
PatrycjaKa92 <patrycja-kawska@wp.pl>
2023-12-16 14:34:01 +0100

    modal after 30 s show

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { ModalComponent } from './modal/modal.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+
   title = 'recuirementTask-Confilogi';
+
+  @ViewChild('modal') Modal!: ModalComponent;
+
+  ngAfterViewInit(): void {
+    setTimeout(()=>{
+      this.Modal.modal();
+      console.log(this.Modal);
+  }, 30000);
+  }
+
 }
